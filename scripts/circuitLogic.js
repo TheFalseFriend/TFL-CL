f936ad7ad8f21bdfb93381fda562ab3d4d7ca669
Thomas Cumming <thomas.cumming18@gmail.com>
2015-11-03 15:50:25 +0000

    Updated js logic to handle adding the new gate svg glyphs

--- scripts/circuitLogic.js
+++ scripts/circuitLogic.js
@@ -1,51 +1,77 @@
-var svgWorkbenchArea = d3.select("#svgDrawArea")
-	            .attr('width', $("#buildArea").width())
-	            .attr('height',  $("#buildArea").height());
+var svgWorkbenchArea = d3.select('#svgDrawArea')
+	            .attr('width', $('#buildArea').width())
+	            .attr('height',  $('#buildArea').height());
 
 	    
 
 	    	var drag = d3.behavior.drag()
-	    		.on("drag", gateMoved)
-	    		.on("dragstart", dragBegun)
-	    		.on("dragend", dragEnded);
+	    		.on('drag', gateMoved)
+	    		.on('dragstart', dragBegun)
+	    		.on('dragend', dragEnded);
           
 
 	    	function gateMoved(d) {
 	    		var x = d3.event.x;
 	    		var y = d3.event.y;
 
-	    		d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
+	    		d3.select(this).attr('x',x)
+	    					   .attr('y',y);
 	    	}
 
 	    	function dragBegun() {
 	    		d3.select(this).transition()
-			      .ease("elastic")
+			      .ease('elastic')
 			      .duration(500)
-			      .attr("r", 48);
+			      .select('path')
+			      .attr('stroke','red')
+			      .attr('stroke-width', '6');
 	    	}
 
 	    	function dragEnded() {
 	    		d3.select(this).transition()
-			      .ease("elastic")
+			      .ease('elastic')
 			      .duration(500)
-			      .attr("r", 30);
+			      .selectAll('g')
+			      .attr('stroke-width', 1);
 	    	}
 
             // Invoke to add a single gate of type 'gateType' to the workbench area
             function addGate(event) {
-            	svgWorkbenchArea.append("circle")
-            		.attr("r", 30)
-            		.attr("class", event.data.gateType)
-            		.attr("transform", "translate(" + $("#svgDrawArea").width() / 2 + "," +  $("#svgDrawArea").height() / 2 + ")")
-            		.call(drag)
+            	switch(event.data.gateType) {
+            		case 'andGate':
+            			svgWorkbenchArea.append('use')
+            							.attr('xlink:href','svgs/gateSVGS.svg#andGateGlyph')
+            							.attr('x',$('#svgDrawArea').width() / 2)
+            							.attr('y',$('#svgDrawArea').height() / 2)
+            							.call(drag);
+            			break;
+
+            		case 'orGate':
+            			svgWorkbenchArea.append('use')
+            							.attr('xlink:href','svgs/gateSVGS.svg#orGateGlyph')
+            							.attr('x',$('#svgDrawArea').width() / 2)
+            							.attr('y',$('#svgDrawArea').height() / 2)
+            							.call(drag);
+            			break;
+            		case 'notGate':
+            			svgWorkbenchArea.append('use')
+            							.attr('xlink:href','svgs/gateSVGS.svg#notGateGlyph')
+            							.attr('x',$('#svgDrawArea').width() / 2)
+            							.attr('y',$('#svgDrawArea').height() / 2)
+            							.call(drag);
+            			break;
+            		default:
+            			alert('dead')
+            			break;
+            	}
             }
 
             function clearWorkbench() {
-            	svgWorkbenchArea.selectAll(".gate").remove();
+            	svgWorkbenchArea.selectAll('use').remove();
             }
 
-            $("#addAND").click({ gateType: 'andGate gate' }, addGate)
-            $("#addOR").click({ gateType: 'orGate gate' }, addGate)
-            $("#addNOT").click({ gateType: 'notGate gate' }, addGate)
+            $('#addAND').click({ gateType:'andGate' }, addGate)
+            $('#addOR').click({ gateType: 'orGate' }, addGate)
+            $('#addNOT').click({ gateType: 'notGate' }, addGate)
 
-            $("#executeClear").click(clearWorkbench)
+            $('#executeClear').click(clearWorkbench)
